0ce0d997c002c9ddcd6cec625f5fdee8887dc316
AnnaSemibabnova <61233581+AnnaSemibabnova@users.noreply.github.com>
2021-05-24 18:24:47 +0300

    lab

---
+++ samples/table.cpp
@@ -0,0 +1,16 @@
+#include <iomanip>
+#include "table.h"
+#include <stack>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  AVL_table<int, int>A2(Node<int, int>(-1, 2));
+  for (int i = 0; i < 10; i++)A2.add(i, i + 1);
+  bool f = A2.find(4);
+  A2.erase(4);
+  cout << (f == A2.find(4));
+}
